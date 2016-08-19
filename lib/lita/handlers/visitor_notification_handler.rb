bcfda0714006f76820a5a69fee3b96557879bbbf
Linmiao Xu <linmiao.xu@gmail.com>
2016-08-19 13:40:14 -0700

    Update message format again

--- lib/lita/handlers/visitor_notification_handler.rb
+++ lib/lita/handlers/visitor_notification_handler.rb
@@ -9,8 +9,8 @@ module Lita
       # header for webhook request
       config :webhook_headers
 
-      route(/^(.+) is here to see you at (.+) - front desk$/, :notify_visitor)
-      route(/^(.+) is here at (.+) - front desk$/, :notify_visitor)
+      route(/^(.+) is here to see you at (.+)\.$/, :notify_visitor)
+      route(/^(.+) is here to see no host at (.+)\.$/, :notify_visitor)
 
       def notify_visitor(response)
         groups = response.matches[0]
