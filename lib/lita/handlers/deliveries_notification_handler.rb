0a2ef1fe6191e1c9c647f12e8b4e99b3d8f8f539
Luis Edymerchk Laverde <edyhack@gmail.com>
2017-12-11 15:41:05 -0500

    ouput

--- lib/lita/handlers/deliveries_notification_handler.rb
+++ lib/lita/handlers/deliveries_notification_handler.rb
@@ -4,15 +4,14 @@ require 'httparty'
 module Lita
   module Handlers
     class DeliveriesNotificationHandler < Handler
-      SUBJECT_REGEX = /delivery/
+      SUBJECT_REGEX = /You have a delivery from (.+)/
 
       route(SUBJECT_REGEX, :notify_delivery)
 
       def notify_delivery(response)
-        response.reply("TEST RESPONSE....")
+        response.reply("TEST RESPONSE -> #{response} -> #{response.matches}")
       end
     end
-    puts "DeliveriesNotificationHandler registerd...."
     Lita.register_handler(DeliveriesNotificationHandler)
   end
 end
