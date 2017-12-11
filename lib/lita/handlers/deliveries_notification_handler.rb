94235ad6c5fff888da907883259f2d674d00741d
Luis Edymerchk Laverde <edyhack@gmail.com>
2017-12-11 15:37:08 -0500

    adding debug information

--- lib/lita/handlers/deliveries_notification_handler.rb
+++ lib/lita/handlers/deliveries_notification_handler.rb
@@ -5,22 +5,14 @@ module Lita
   module Handlers
     class DeliveriesNotificationHandler < Handler
       SUBJECT_REGEX = /delivery/
-      # URL to webhook when we see a visitor noticiation
-      config :webhook_url
-      # header for webhook request
-      config :webhook_headers
 
       route(SUBJECT_REGEX, :notify_delivery)
 
       def notify_delivery(response)
-        HTTParty.post(
-          "https://requestb.in/1jc4kro1",
-          body: response
-        )
-        response.reply(response)
+        response.reply("TEST RESPONSE....")
       end
     end
-
+    puts "DeliveriesNotificationHandler registerd...."
     Lita.register_handler(DeliveriesNotificationHandler)
   end
 end
