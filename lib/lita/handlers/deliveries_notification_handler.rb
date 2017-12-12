cadbabeeb801fe24c07773e8092d4c05ed8898fb
Luis Edymerchk Laverde <edyhack@gmail.com>
2017-12-12 11:33:04 -0500

    replace regex

--- lib/lita/handlers/deliveries_notification_handler.rb
+++ lib/lita/handlers/deliveries_notification_handler.rb
@@ -4,12 +4,12 @@ require 'httparty'
 module Lita
   module Handlers
     class DeliveriesNotificationHandler < Handler
-      SUBJECT_REGEX = /You have a delivery from (.+)/
+      SUBJECT_REGEX = /You have a delivery from/
 
       route(SUBJECT_REGEX, :notify_delivery)
 
       def notify_delivery(response)
-        response.reply("TEST RESPONSE -> #{response.message.body} -> #{response.message.extensions}")
+        response.reply("TEST RESPONSE -> #{response.message} -> #{response.message.body} -> #{response.message.extensions}")
       end
     end
     Lita.register_handler(DeliveriesNotificationHandler)
